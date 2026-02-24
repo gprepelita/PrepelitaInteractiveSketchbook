4cd2afa4be5ea5ece5bf0f59ec082eb140dbeef7
prepatronic <gprepelita@sfsu.edu>
2026-02-24 13:27:08 -0800

    modified:   .DS_Store 	new file:   Sketch Number 10/.DS_Store 	new file:   Sketch Number 10/js/sketch10.js 	new file:   Sketch Number 10/number10.html 	modified:   index.html

---
+++ Sketch Number 10/js/sketch10.js
@@ -0,0 +1,27 @@
+let offset = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //RECT MODE CENTER measures placement from the center of the shape
+  rectMODE(CENTER);
+}
+
+function draw() {
+  background(0);
+
+  push();
+  translate(200, 0);
+  fill(100, 200, 100);
+  rect(width / 3, height / 2, 100, 100);
+  pop();
+
+  rect((width * 2) / 3, height / 2, 100, 100);
+}
+
+function keyPressed() {
+  offset += 20;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
