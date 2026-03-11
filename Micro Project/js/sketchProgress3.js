24216f9beb8e72dae69b944aac39952baf630a98
prepatronic <gprepelita@sfsu.edu>
2026-03-10 17:27:06 -0700

    modified:   Micro Project/.DS_Store 	new file:   Micro Project/fonts/Qaaxee.ttf 	new file:   Micro Project/img/.DS_Store 	new file:   Micro Project/img/molesprite.png 	new file:   Micro Project/img/molesprite.svg 	new file:   Micro Project/img/wormsprite.png 	new file:   Micro Project/img/wormsprite.svg 	modified:   Micro Project/js/sketchProgress3.js 	modified:   Micro Project/microProjectProgress3.html 	new file:   img/.DS_Store

--- Micro Project/js/sketchProgress3.js
+++ Micro Project/js/sketchProgress3.js
@@ -1,88 +1,116 @@
 let gameStarted = false;
-let circleX = 650;
-let circleY = 300;
-let circleRadius = 25;
-let boxOneX = 600;
-let boxOneY = 50;
-let boxOneW = 100;
-let boxOneH = 300;
+let moleX = 827;
+let moleY = 300;
+let moleAngle = 0;
+let mole;
+let worm;
+let font;
 
-let boxTwoX = 600;
-let boxTwoY = 50;
-let boxTwoW = 300;
-let boxTwoH = 100;
+// Store all maze boxes in one array
+const boxes = [
+  { x: 800, y: 50, w: 150, h: 400 }, //box 1
+  { x: 100, y: 50, w: 850, h: 150 }, //box 2
+  { x: 100, y: 50, w: 150, h: 850 }, //box 3
+  { x: 100, y: 750, w: 400, h: 150 }, //box 4
+  { x: 350, y: 550, w: 150, h: 350 }, //box 5
+  { x: 350, y: 550, w: 600, h: 150 }, //box 6
+  { x: 800, y: 550, w: 150, h: 350 }, //box 7
+  { x: 800, y: 750, w: 500, h: 150 }, //box 8
+  { x: 1150, y: 50, w: 150, h: 850 }, //box 9
+  { x: 1150, y: 50, w: 400, h: 150 }, //box 10
+  { x: 1400, y: 50, w: 150, h: 500 }, //box 11
+  { x: 1400, y: 400, w: 400, h: 150 }, //box 12
+];
 
-// Creating a function to check if the circle is inside either of the boxes
-function insideBox(x, y) {
-  let inBoxOne =
-    x > boxOneX + circleRadius &&
-    x < boxOneX + boxOneW - circleRadius &&
-    y > boxOneY + circleRadius &&
-    y < boxOneY + boxOneH - circleRadius;
-
-  let inBoxTwo =
-    x > boxTwoX + circleRadius &&
-    x < boxTwoX + boxTwoW - circleRadius &&
-    y > boxTwoY + circleRadius &&
-    y < boxTwoY + boxTwoH - circleRadius;
-
-  return inBoxOne || inBoxTwo;
+function preload() {
+  mole = loadImage("img/molesprite.svg");
+  worm = loadImage("img/wormsprite.svg");
+  font = loadFont("fonts/Qaaxee.ttf");
 }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES); // changing angle mode to degrees for easier rotation
+  mole.resize(100, 0); // Resizing sprite to fit better in the maze boxes
+}
+
+// Check if (x,y) is inside ANY box in the array
+function insideBox(x, y) {
+  return boxes.some(
+    (b) =>
+      x > b.x &&
+      x < b.x + b.w - mole.width &&
+      y > b.y &&
+      y < b.y + b.h - mole.height,
+  );
 }
 
 function draw() {
-  background(0);
+  background(64, 42, 34);
+  gameStarted ? drawGame() : drawMenu();
+}
 
-  // Drawing the maze region
-  if (!gameStarted) {
-    fill(255);
-    noStroke();
-    strokeWeight(2);
-    rect(boxOneX, boxOneY, boxOneW, boxOneH);
-    rect(boxTwoX, boxTwoY, boxTwoW, boxTwoH);
-    textSize(32);
-    fill(255);
-    textAlign(CENTER, CENTER);
-    text(
-      "Escape the maze in time\nMove with arrow keys\nClick to Start",
-      width / 6,
-      height / 4,
-    );
-  }
-
-  // Drawing circle
+function drawGame() {
+  // Drawing all boxes using a loop
+  fill(115, 83, 64);
   noStroke();
-  fill(200, 50, 50);
-  ellipse(circleX, circleY, circleRadius);
+  for (let b of boxes) {
+    rect(b.x, b.y, b.w, b.h);
+  }
+
+  // Movement with WASD keys
+  let speed = 5;
+  let nextX = moleX,
+    nextY = moleY;
+
+  if (keyIsDown(65)) {
+    nextX -= speed;
+    moleAngle = 180;
+  } // A
+  if (keyIsDown(68)) {
+    nextX += speed;
+    moleAngle = 0;
+  } // D
+  if (keyIsDown(87)) {
+    nextY -= speed;
+    moleAngle = 270;
+  } // W
+  if (keyIsDown(83)) {
+    nextY += speed;
+    moleAngle = 90;
+  } // S
+
+  if (insideBox(nextX, nextY)) {
+    moleX = nextX;
+    moleY = nextY;
+  }
+
+  // Drawing mole
+  push();
+  translate(moleX + mole.width / 2, moleY + mole.height / 2);
+  rotate(moleAngle + 90);
+  imageMode(CENTER);
+  image(mole, 0, 0);
+  pop();
+
+  // Drawing worm
+  worm.resize(40, 0);
+  image(worm, width - 60, height / 2);
 }
 
-function keyPressed() {
-  // only allow movement after the game has started
-  if (!gameStarted) {
-    return;
-  }
-
-  let newX = circleX;
-  let newY = circleY;
-
-  if (keyCode === LEFT_ARROW) {
-    newX -= 10;
-  } else if (keyCode === RIGHT_ARROW) {
-    newX += 10;
-  } else if (keyCode === UP_ARROW) {
-    newY -= 10;
-  } else if (keyCode === DOWN_ARROW) {
-    newY += 10;
-  }
-
-  if (insideBox(newX, newY)) {
-    circleX = newX;
-    circleY = newY;
-  }
+function drawMenu() {
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textFont(font);
+  textSize(100);
+  text("Mole Run!", width / 2, 100);
+  textSize(32);
+  text(
+    "Catch the worm in time!\nMove with WASD keys\nClick to Start",
+    width / 2,
+    height / 2,
+  );
 }
-// Circle movement (only if the new position is inside the boxes)
 
 function mousePressed() {
   gameStarted = true;
